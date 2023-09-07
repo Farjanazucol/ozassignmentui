82668dea227f50519c28ae9a088cd9ee52446f90
Farjanazucol <farjana.fatehmohd@zucolservices.com>
2023-09-07 12:52:13 +0530

    Initial commit

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -200,14 +200,14 @@ export default function Example() {
               />
             </Link>
           </div>
-          <div className="flex lg:hidden">
+          <div className="flex lg:hidden -mt-10">
             <button
               type="button"
-              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              className="  inline-flex items-center justify-center rounded-md  text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
-              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              <Bars3Icon className="h-6 w-6 " aria-hidden="true" />
             </button>
           </div>
           <Popover.Group className="hidden lg:flex md:gap-x-10 xl:gap-x-24 lg:gap-x-12">
